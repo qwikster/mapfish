b7c023303dfc19a739ed83d422db854fea17f813
qwikster <ocoincs@gmail.com>
2025-11-26 21:25:06 -0500

    renderring properly

--- src/mapfisher/main.py
+++ src/mapfisher/main.py
@@ -1,17 +1,19 @@
 # Main entry point
 from mapfisher.map import render_map
+import random
 
-#lat = 35.980056
-#lon = 139.752389
-lat, lon = 44.923095, -77.362277
+# lat, lon = random.uniform(-90, 90), random.uniform(-180, 180)
+# lat, lon = 35.980056, 139.752389
+lat, lon = 44.505245, -77.472601
+# lat, lon = 44.000000, -77.000000
+
 # REPLACE THESE
 # these are the coordinates from that one part in the lucky star op
 # also should be challenging to render because japan
 
 def entry():
-    data = render_map(lat, lon)
-    print(data)
-    input()
+    for i in range(10, 19):
+        render_map(lat, lon, zoom = i)
 
 if __name__ == "__main__":
     entry()
--- src/mapfisher/map.py
+++ src/mapfisher/map.py
@@ -1,10 +1,11 @@
 # Actual map rendering
 
 import staticmaps # py-staticmaps
-from img2unicode import FastGenericDualOptimizer, Renderer
+from img2unicode import FastGenericDualOptimizer, FastQuadDualOptimizer, HalfBlockDualOptimizer, Renderer
 from PIL import ImageDraw
 from io import BytesIO, StringIO
 import sys
+import os
 import time
 
 def textsize(self, text, font=None):
@@ -13,24 +14,42 @@ def textsize(self, text, font=None):
     
 ImageDraw.ImageDraw.textsize = textsize
 
-def render_map(lat, lon, zoom=16, width=800, height=400): # TODO: make dynamic/configurable later - Q/E keys?
+def render_map(lat, lon, zoom=14, width=640, height=360): # TODO: make dynamic/configurable later - Q/E keys?
     start_time = time.perf_counter()
     
     context = staticmaps.Context()
-    ctx_time = time.perf_counter()
-    
     context.set_tile_provider(staticmaps.tile_provider_ArcGISWorldImagery) # try other ones later this one definitely works
     center = staticmaps.create_latlng(lat, lon)
-    setting_time = time.perf_counter()
     
     context.set_center(center)
     context.set_zoom(zoom)
-    zoom_time = time.perf_counter()
+    setup_time = time.perf_counter()
     
     image = context.render_pillow(width, height)
     maprend_time = time.perf_counter()
     
-    optimizer = FastGenericDualOptimizer("block") # also consider changing render types
-    renderer = Renderer(default_optimizer = optimizer, max_h = 120, max_w = 240) # will need to ebe changed for gamma variants
+    fd = sys.stdout.fileno()
+    columns, rows = os.get_terminal_size(fd)
+    max_w = columns - 2
+    max_h = int(rows * 0.5) - 5
     
-    renderer.render_terminal(image, sys.stdout, optimizer = optimizer)
+    optimizer = FastGenericDualOptimizer("block") # also consider changing render types
+    renderer = Renderer(default_optimizer = optimizer, max_w = max_w, max_h = max_h) # will need to ebe changed for gamma variants
+    setrend_time = time.perf_counter()
+    
+    out = StringIO()
+    renderer.render_terminal(image, out, optimizer = optimizer)
+    final_time = time.perf_counter()
+    
+    data = out.getvalue()
+    print("\x1b[H\x1b[2J")
+    print(data)
+    
+    print("lat/long: ", lat, lon, "\nzoom: ", zoom, "\nsize: ", width, height)
+    print("Setup:       ", setup_time - start_time)
+    print("Map render:  ", maprend_time - setup_time)
+    print("Rend setup:  ", setrend_time - maprend_time)
+    print("img2unicode: ", final_time - setrend_time)
+    print("Elapsed:     ", final_time - start_time)
+    
+    return data
